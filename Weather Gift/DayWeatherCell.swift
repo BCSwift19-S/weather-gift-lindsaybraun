e6ec703076e02846dea59eaf3830585f81d23092
Lindsay Braun <lindsaybraun@lindsays-macbook-pro.local>
2019-03-23 19:32:48 -0400

    7.20 Completed

---
+++ Weather Gift/DayWeatherCell.swift
@@ -0,0 +1,23 @@
+//
+//  DayWeatherCell.swift
+//  Weather Gift
+//
+//  Created by Lindsay Braun on 3/23/19.
+//  Copyright © 2019 Lindsay Braun. All rights reserved.
+//
+
+import UIKit
+
+class DayWeatherCell: UITableViewCell {
+    @IBOutlet weak var dayCellIcon: UIImageView!
+    @IBOutlet weak var dayCellWeekday: UILabel!
+    @IBOutlet weak var dayCellMaxTemp: UILabel!
+    @IBOutlet weak var dayCellMinTemp: UILabel!
+    @IBOutlet weak var dayCellSummary: UITextView!
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
--- Weather Gift/DetailVC.swift
+++ Weather Gift/DetailVC.swift
@@ -15,6 +15,7 @@ class DetailVC: UIViewController {
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var summaryLabel: UILabel!
     @IBOutlet weak var currentImage: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
     
     var currentPage = 0
     var locationsArray = [WeatherLocation]()
@@ -23,6 +24,8 @@ class DetailVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         if currentPage != 0 {
             self.locationsArray[currentPage].getWeather {
                 self.updateUserInterface()
@@ -105,5 +108,19 @@ extension DetailVC: CLLocationManagerDelegate{
     }
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Failed to get user location.")
+    }
+}
+
+extension DetailVC: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 7
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DayWeatherCell", for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
     }
 }
